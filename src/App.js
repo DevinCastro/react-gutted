e34e9d5eec4cf7f7e7f4612c678f494329679731
Devin Castro <dcrocker21@gmail.com>
2020-09-14 14:28:34 -0700

    funtional profile

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Nav from './components/Nav'
+import Jumbotron from './components/Jumbotron'
+import Form from './components/Form'
 
 function App() {
   return (
-    
+    <>
+      <Nav />
+      <Jumbotron />
+      <Form />
+    </>
   );
 }
 
---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Jumbotron = () => {
+  return (
+    <div className="jumbotron">
+      <h1 className="display-4">Hello, Im Kenny!</h1>
+      <p className="lead">I refuse to die.  Ever.</p>
+      <hr className="my-4"/>
+        <p>I am the route building master.</p>
+        <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </div>
+  )
+}
+
+export default Jumbotron
